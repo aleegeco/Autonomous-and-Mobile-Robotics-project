e4e79864e8a9ea33791c100f63e36c392b67068c
aleegeco <byparahz@gmail.com>
2022-07-19 23:40:50 +0200

    prove ale

--- src/laser_value/src/visualization_2_class.py
+++ src/laser_value/src/visualization_2_class.py
@@ -17,12 +17,15 @@ map_path = map_dir + '/' + map
 
 class sanitizer_node():
     def __init__(self):
-        self.occupancy = OccupancyGrid(header=rospy.Header()) # initialize map object
-
         map_dir = os.path.dirname(os.path.realpath(map))
         map = 'map_02.pgm'
         map_path = map_dir + '/' + map
         self.map_img = cv2.imread(map_path, cv2.IMREAD_GRAYSCALE)
+
+        self.occupancy = OccupancyGrid(header=rospy.Header()) # initialize map object
+        self.grid_cell = GridCells(header=rospy.Header())
+        self.uv_store = np.zeros((len(self.map_img), len(self.map_img)))
+
         self.res = 0.2 # map resolution
         self.map_offset = 10 # difference between real map and discretized one (used to match the two better)
 
@@ -34,8 +37,18 @@ class sanitizer_node():
         self.occupancy.info.origin.position.x = -self.map_offset - self.res
         self.occupancy.info.origin.position.y = -self.map_offset - self.res
 
-    def UV_energy(self, data):
+    def UV_energy(self, pos, data):
         Pl = 1e-4 # light power [Wm^2]
+        x_turtle = pos.x
+        y_turtle = pos.y
+
+        x_pixel = data.x
+        y_pixel = data.y
+
+        if x_pixel == x_turtle and y_pixel == y_turtle:
+            uv_dt = 0
+        else:
+            uv_dt = Pl / (np.square((x_pixel - x_turtle)*self.res) * np.square((y_pixel - y_turtle))*self.res)
 
 
 
